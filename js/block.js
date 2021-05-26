bdcb4771fabe81f74b68dd234e27966d8e7cab31
Devang100 <81232858+Devang100@users.noreply.github.com>
2021-05-26 16:29:54 +0530

    Add files via upload

---
+++ js/block.js
@@ -0,0 +1,36 @@
+class Block{
+  constructor(x, y, width, height) {
+      var options = {
+        restitution :0,
+        friction :1,       
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      this.image = loadImage("images/bottle.png")
+      World.add(world, this.body);
+      this.Visibility = 255;
+    }
+    display(){
+      if(this.body.speed<5){
+         var angle = this.body.angle;
+         push();
+         translate(this.body.position.x, this.body.position.y);
+         rotate(angle);
+         imageMode(CENTER);
+         image(this.image,0,0,80,50);
+         pop(); 
+      }
+      else{
+         World.remove(world,this.body);
+         push();
+         this.Visibility-=7;
+         tint(255,this.Visibility);
+         imageMode(CENTER);
+         image(this.image,this.body.position.x,this.body.position.y,80,50);
+         pop();
+      }
+      
+    }
+}
+
